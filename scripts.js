23991e62b310a7336507ee0665c3e04bd5417fca
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-03-02 08:51:50 -0300

    Conclui a parte funcional do projeto

--- scripts.js
+++ scripts.js
@@ -57,7 +57,9 @@ function inserirImagem() {
   ativaConfirmarPedido();
 }
 function ativaConfirmarPedido() {
+  let botao = document.querySelector("button");
   if (
+    isValidHttpUrl(pedido.image) &&
     pedido.model != "" &&
     pedido.neck != "" &&
     pedido.material != "" &&
@@ -65,20 +67,65 @@ function ativaConfirmarPedido() {
     pedido.owner != "" &&
     pedido.author != ""
   ) {
-    let botao = document.querySelector("button");
     botao.classList.add("liberado");
+  } else {
+    botao.classList.remove("liberado");
   }
 }
 function confirmarPedido() {
-    console.log(pedido);
-    const promessa=axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", pedido);
-    promessa.then(tratarSucesso);
-    promessa.catch(tratarErro);
-    function tratarSucesso(sucesso){
-        console.log(sucesso);
-        alert("Encomenda confirmada");
-    }
-    function tratarErro(erro){
-        console.log(erro);
-    }
+  const promessa = axios.post(
+    "https://mock-api.driven.com.br/api/v4/shirts-api/shirts",
+    pedido
+  );
+  promessa.then(tratarSucesso);
+  promessa.catch(tratarErro);
+  function tratarSucesso(sucesso) {
+    alert("Encomenda confirmada");
+    carregarUltimosPedidos();
+  }
+  function tratarErro(erro) {
+    alert("Ops, não conseguimos processar sua encomenda");
+  }
 }
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+    console.log(url);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+function carregarUltimosPedidos() {
+  const ultimos = document.querySelector(".pedidos");
+  const promessa = axios.get(
+    "https://mock-api.driven.com.br/api/v4/shirts-api/shirts"
+  );
+  promessa.then(tratarSucesso);
+  function tratarSucesso(sucesso) {
+    ultimos.innerHTML = "";
+    for (let i = 0; i < sucesso.data.length; i++) {
+      ultimos.innerHTML += `
+      <div class="pedido" onclick="pedirUltimoPedido('${sucesso.data[i].model}','${sucesso.data[i].neck}','${sucesso.data[i].material}','${sucesso.data[i].image}','${sucesso.data[i].owner}')">
+        <img src="${sucesso.data[i].image}"/>
+      <p><b>Criador:</b> ${sucesso.data[i].owner}</p>
+    </div>
+    `;
+    }
+  }
+}
+function pedirUltimoPedido(modelo, gola, material, imagem, proprietario) {
+  pedido.model = modelo;
+  pedido.neck = gola;
+  pedido.material = material;
+  pedido.image = imagem;
+  pedido.owner = proprietario;
+  pedido.author = nome;
+  if (window.confirm("Você realmente quer fazer esse pedido?")) {
+    confirmarPedido();
+  }
+}
+carregarUltimosPedidos();
